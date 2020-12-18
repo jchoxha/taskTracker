07859c37cc9f740de9824487ad9c06891e463c23
jchoxha <jeton_hoxha@student.uml.edu>
2020-12-18 14:28:37 -0500

    Continued working on random color for task list items, added border style to new task form

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,31 @@ import TaskDetails from './TaskDetails';
 import { TaskContext } from '../contexts/TaskContext';
 
 // const randomBackgroundColor = () => {
-//   const color = Math.floor(Math.random() * Math.floor(10));
+//   const colorNumber = Math.floor(Math.random() * Math.floor(6));
+
+//   var color = 'red';
+//   switch (colorNumber) {
+//     case 0:
+//       color = "blue";
+//       break;
+//     case 1:
+//       color = "red";
+//       break;
+//     case 2:
+//        color = "green";
+//       break;
+//     case 3:
+//       color = "yellow";
+//       break;
+//     case 4:
+//       color = "purple";
+//       break;
+//     case 5:
+//       color = "orange";
+//       break;
+//     case 6:
+//       color = "pink";
+//   }
 
 //   return color;
 // }
@@ -11,7 +35,9 @@ import { TaskContext } from '../contexts/TaskContext';
 const TaskList = () => {
   const { tasks } = useContext(TaskContext);
   return tasks.length ? (
-    <div className="task-list">
+    <div className="task-list" 
+    // style={{ backgroundColor: randomBackgroundColor()}}
+    >
       <ul>
         {tasks.map(task => {
           return ( <TaskDetails task={task} key={task.id} /> );
